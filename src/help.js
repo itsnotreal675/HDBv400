a797d42686df2774da6ddf19066a332d58566488
itsnotreal675 <76721666+itsnotreal675@users.noreply.github.com>
2020-12-29 23:22:32 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,28 @@
+const help = () => {
+	return '> 𝐂𝐨𝐦𝐚𝐧𝐝𝐨𝐬 𝐩𝐚𝐫𝐚 𝐟𝐚𝐳𝐞𝐫 𝐟𝐢𝐠𝐮𝐫𝐢𝐧𝐡𝐚𝐬 <\n' +
+	'𝙲𝚘𝚖𝚊𝚗𝚍𝚘 : *!sticker* ou *!stiker*\n' +
+	'Dᴇsᴄʀɪᴄ̧ᴀ̃ᴏ : converta imagens/gifs da ana/video para figurinhas\n' +
+	'𝙲𝚘𝚖𝚘 𝚞𝚜𝚊𝚛 : marque a imagem/gif/video, ou envie imagem/gif/video com a legenda descrita acima\n\n' + 
+	'𝙲𝚘𝚖𝚊𝚗𝚍𝚘 : *!toimg*\n' +
+	'Dᴇsᴄʀɪᴄ̧ᴀ̃ᴏ : converta figurinhas para imagem\n' +
+	'𝙲𝚘𝚖𝚘 𝚞𝚜𝚊𝚛 : marque a figurinha com a legenda descrita acima\n\n' +
+	'𝙲𝚘𝚖𝚊𝚗𝚍𝚘 (*off no momento*) : *!tsticker* ou *!tstiker*\n' + 
+	'Dᴇsᴄʀɪᴄ̧ᴀ̃ᴏ : converta texto para figurinha (*off no momento*)\n' +
+	'𝙲𝚘𝚖𝚘 𝚞𝚜𝚊𝚛 : *!tsticker* (coloque o texto aqui)\n\n' +
+'> 𝐂𝐨𝐦𝐚𝐧𝐝𝐨𝐬 𝐝𝐞 𝐦𝐞𝐦𝐞 (*off no momento*)<\n' +
+	'comando : *!meme*\n' +
+	'Dᴇsᴄʀɪᴄ̧ᴀ̃ᴏ : meme aleatório da gringa [english]\n' +
+	'𝙲𝚘𝚖𝚘 𝚞𝚜𝚊𝚛 : basta enviar o comando\n\n' +
+	'𝙲𝚘𝚖𝚊𝚗𝚍𝚘 : *!memebr*\n' +
+	'Dᴇsᴄʀɪᴄ̧ᴀ̃ᴏ : memes aleatórios br [pt-br]\n' +
+	'𝙲𝚘𝚖𝚘 𝚞𝚜𝚊𝚛 : basta enviar o comando\n\n' +
+'> 𝐂𝐨𝐦𝐚𝐧𝐝𝐨 𝐝𝐞 𝐭𝐞𝐱𝐭𝐨 𝐩𝐚𝐫𝐚 𝐟𝐚𝐥𝐚 <\n' +
+	'𝙲𝚘𝚖𝚊𝚗𝚍𝚘 : *!gtts*\n' +
+	'Dᴇsᴄʀɪᴄ̧ᴀ̃ᴏ : conversor de texto para audio\n' +
+	'𝙲𝚘𝚖𝚘 𝚞𝚜𝚊𝚛 : *!gtts [cc] [texto]*\nexemplo : *!gtts pt-br Admira brow?*'
+	
+"𝙱𝚘𝚝 𝚊𝚒𝚗𝚍𝚊 𝚎𝚖 𝚍𝚎𝚜𝚎𝚗𝚟𝚘𝚕𝚟𝚒𝚖𝚎𝚗𝚝𝚘, 𝚒𝚗𝚜𝚝𝚊𝚋𝚒𝚕𝚒𝚍𝚊𝚍𝚎 𝚗𝚘 𝚜𝚎𝚛𝚟𝚒𝚍𝚘𝚛 𝚜𝚊𝚘 𝚗𝚘𝚛𝚖𝚊𝚒𝚜."
+  𝗠𝗲𝗻𝘂 𝗛𝗗𝗕 𝗕𝗢𝗧𝘃𝟭.𝟯  🤡😳
+}
+
+exports.help = help
